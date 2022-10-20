fa02591699002a19703cf5d33cf92faf5bcf5bc6
dasommmm <103475183+dasommmm@users.noreply.github.com>
2022-10-20 19:36:29 +0900

    10.20

---
+++ js/index.js
@@ -0,0 +1,26 @@
+scrollHeader();
+goToTop();
+
+function scrollHeader(){
+    const header = document.getElementsByTagName('header')[0];
+
+    window.addEventListener('mousewheel' , (e) => {
+        // console.log(e.deltaY);
+        // 0보다 작을때 위로 올라감 (header 나타나게)
+        if(e.deltaY<0){
+            header.classList.remove('header-hide');
+        }else{
+            header.classList.add('header-hide');
+        }
+    })
+}
+
+
+function goToTop(){
+    const topBut = document.querySelector('.top');
+
+    topBut.addEventListener('click', (e) =>{
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    });
+
+}
