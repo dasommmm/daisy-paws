f3062dc474bbf71069ec8692ed126590da5c6105
dasommmm <103475183+dasommmm@users.noreply.github.com>
2022-10-21 21:05:20 +0900

    10.21-1

--- js/index.js
+++ js/index.js
@@ -1,19 +1,36 @@
 scrollHeader();
 goToTop();
 iconUp();
+moveUp();
 
 function scrollHeader(){
     const header = document.getElementsByTagName('header')[0];
 
-    window.addEventListener('mousewheel' , (e) => {
-        // console.log(e.deltaY);
-        // 0보다 작을때 위로 올라감 (header 나타나게)
-        if(e.deltaY<0){
-            header.classList.remove('header-hide');
-        }else{
+    let preScroll = 0;
+    window.addEventListener('scroll', (e) => {
+        // console.log(window.scrollY);
+        const nowScroll = window.scrollY;
+        const direction = preScroll<nowScroll //스크롤 밑으로 내렸을때
+        // console.log(direction)
+        if(direction){
             header.classList.add('header-hide');
+        }else{
+            header.classList.remove('header-hide');
         }
+        
+        preScroll=nowScroll;
     })
+
+    // }
+    // window.addEventListener('mousewheel' , (e) => {
+    //     // console.log(e.deltaY);
+    //     // 0보다 작을때 위로 올라감 (header 나타나게)
+    //     if(e.deltaY<0){
+    //         header.classList.remove('header-hide');
+    //     }else{
+    //         header.classList.add('header-hide');
+    //     }
+    // })
 }
 
 
@@ -28,9 +45,43 @@ function goToTop(){
 
 
 function iconUp(){
-    const iconCon = document.getElementsByClassName('icon-con')
+    const summary = document.getElementById('summary')
     const summaryIcon = document.getElementsByClassName('summary-icon')
 
-    console.log(iconCon[0].getBoundingClientRect().top)
-    console.log(window.innerHeight)
+    // console.log(window.innerHeight)
+    // console.log(iconCon.length)
+    
+    for(let i=0; i<summaryIcon.length; i++){ //summaryIcon.length이부분 맞는지
+       
+        window.addEventListener('scroll',(e)=>{
+            // console.log(summary.getBoundingClientRect().top-window.innerHeight/3)
+
+            if(summary.getBoundingClientRect().top-window.innerHeight/3<0){
+                summaryIcon[i].classList.add('show');
+            }else{
+                summaryIcon[i].classList.remove('show');
+            }
+
+        })
+
+    }
+
+}
+
+function moveUp(){
+    const sectionMove = document.getElementsByClassName('section-move');
+    const mockup = document.getElementsByClassName('wrapper');
+
+    for(let i=0; i<sectionMove.length; i++){
+        
+        window.addEventListener('scroll', (e)=>{
+            console.log(sectionMove[0].getBoundingClientRect().top);
+
+            if(sectionMove[0].getBoundingClientRect().top < 0){
+                mockup[i].classList.add('move-up')
+            }else{
+                mockup[i].classList.remove('move-up')
+            }
+        })
+    }
 }
